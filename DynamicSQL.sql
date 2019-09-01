bc9446ef74b9fec1f525a155788c5b336c11d346
anowereng <anowereng@gmail.com>
2019-09-01 23:06:42 +0600

    Update DynamicSQL.sql

--- DynamicSQL.sql
+++ DynamicSQL.sql
@@ -5,3 +5,10 @@ set @sql= 'Select * from tblCat_Supplier '+'where Email = @Email'
 set @params= '@Email nvarchar(100)'
 
 exec sp_executesql @sql, @params, @Email='a@mail.vom'
+--exec sp_executesql @sql, @params, 'a@mail.vom'
+	/*  ============== NOTE : Start =============
+		to execute the dynamic sql use systemprocedure sp_executesql . it takse two pre-defined 
+		@statement = the first parameter which is mandatory
+		@params = this is the second parameter , its used to declare paramter 
+	 ============== NOTE : End ============= */ 
+
